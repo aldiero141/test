70008af7333e77443c8af1bc587794f6b6102e03
aldiero141 <rivelinoaldo@gmail.com>
2022-02-07 14:49:30 +0700

    feat: add profile layout

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,6 +1,38 @@
 <template>
-    <div>
-        profile
+    <div class="d-flex flex-column mt-4">
+        <v-row class="align-center">
+             <Card>
+                Banner
+            </Card>
+        </v-row>
+        <v-row class="mt-4">
+            <v-col>
+                <v-row>
+                    <v-avatar size="160" class="mx-auto">
+                        <img
+                            src="https://cdn.vuetifyjs.com/images/john.jpg"
+                            alt="John"
+                            width="100%"
+                        >
+                    </v-avatar>
+                </v-row>
+                <v-row>
+                    <Card>
+                        Banner
+                    </Card>
+                </v-row>
+            </v-col>
+            <v-col >
+                <Card>
+                    Banner
+                </Card>
+            </v-col>    
+            <v-col class="mr-n3">
+                <Card>
+                    Banner
+                </Card>
+            </v-col>    
+        </v-row>
     </div>
 </template>
 
@@ -10,3 +42,4 @@ export default {
     layout: 'dashboard',
 }
 </script>
+
