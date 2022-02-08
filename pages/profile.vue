b2ed9eb23353fd8de53f309edfd064ffd3fdc0f3
aldiero141 <rivelinoaldo@gmail.com>
2022-02-08 16:08:41 +0700

    feat: add card template profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,14 +1,12 @@
 <template>
-    <div class="d-flex flex-column mt-4">
-        <v-row class="align-center">
-             <Card>
-                Banner
-            </Card>
+    <div class="d-flex flex-column my-10" style="height: 100vh">
+        <v-row class="align-center mb-4">
+            <Card style="background: url('https://picsum.photos/id/11/500/300') no-repeat; background-position: center; background-size: cover; height: 30vh;"/>
         </v-row>
         <v-row class="mt-4">
-            <v-col>
+            <v-col class="mr-4">
                 <v-row>
-                    <v-avatar size="160" class="mx-auto">
+                    <v-avatar size="160" class="mx-auto" style="z-index: 1">
                         <img
                             src="https://cdn.vuetifyjs.com/images/john.jpg"
                             alt="John"
@@ -17,19 +15,111 @@
                     </v-avatar>
                 </v-row>
                 <v-row>
-                    <Card>
-                        Banner
+                    <Card style="padding-top: 8em; margin-top: -5em">
+                        <v-row class="ml-12" >
+                            <p>Name :</p>
+                            <v-spacer/>
+                            <p class="mr-16"> Test</p>
+                        </v-row>
+                        <v-row class="ml-12">
+                            <p>Gender :</p>
+                            <v-spacer/>
+                            <p class="mr-16"> Test</p>
+                        </v-row>
+                        <v-row class="ml-12">
+                            <p>Birthday :</p>
+                            <v-spacer/>
+                            <p class="mr-16"> Test</p>
+                        </v-row>
+                        <v-row class="ml-12">
+                            <p>Hometown :</p>
+                            <v-spacer/>
+                            <p class="mr-16"> Test</p>
+                        </v-row>
+                        <v-row class="ml-12">
+                            <p>Bio :</p>
+                            <v-spacer/>
+                            <p class="mr-16"> Test</p>
+                        </v-row>
+                        <v-btn 
+                            flat 
+                            block 
+                            color="green" 
+                            class="mt-8"
+                        >Update Profile</v-btn>
                     </Card>
                 </v-row>
             </v-col>
             <v-col >
-                <Card>
-                    Banner
+                <Card class="pt-4">
+                    <h1 class="mb-8">Career</h1>
+                    <v-row class="ml-12" >
+                        <p>Name :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Gender :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Birthday :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Hometown :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Bio :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-btn 
+                        flat 
+                        block 
+                        color="green" 
+                        class="mt-8"
+                    >Update Career</v-btn>
                 </Card>
             </v-col>    
             <v-col class="mr-n3">
-                <Card>
-                    Banner
+                <Card class="pt-4">
+                    <h1 class="mb-8">Education</h1>
+                    <v-row class="ml-12" >
+                            <p>Name :</p>
+                            <v-spacer/>
+                            <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Gender :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Birthday :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Hometown :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-row class="ml-12">
+                        <p>Bio :</p>
+                        <v-spacer/>
+                        <p class="mr-16"> Test</p>
+                    </v-row>
+                    <v-btn 
+                        flat 
+                        block 
+                        color="green" 
+                        class="mt-8"
+                    >Update Education</v-btn>
                 </Card>
             </v-col>    
         </v-row>
