a6f4f9829dc331d83beb94bdcf66bc4380e9b73a
aldiero141 <rivelinoaldo@gmail.com>
2022-02-08 13:58:01 +0700

    feat: add modal component

---
+++ components/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <v-dialog
+            v-model="show"
+            width="500"
+        >
+            <Card>
+                <slot/>
+            </Card>    
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ModalComponent',
+        props: {
+            value: Boolean
+        },
+        computed: {
+            show: {
+                get () {
+                    return this.value
+                },
+                set (value) {
+                    this.$emit('input', value)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.container{
+    position: absolute;
+    top: 4em;
+    width: 100vw;
+    height: 40vh;
+}
+</style>
