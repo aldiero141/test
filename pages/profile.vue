132de61219b1a58a1b4165032155f6b556fe7286
aldiero141 <rivelinoaldo@gmail.com>
2022-02-11 15:56:22 +0700

    feat: get profile api data binding

--- pages/profile.vue
+++ pages/profile.vue
@@ -13,44 +13,52 @@
         "
       />
     </v-row>
-    {{ user }}
-    <v-row class="mt-4">
+    <v-row class="mt-4" style="height: 100vh">
       <v-col class="mr-4">
         <v-row>
           <v-avatar size="160" class="mx-auto" style="z-index: 1">
             <img
-              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              :src="
+                user.user_image
+                  ? user.user_image
+                  : 'https://cdn.vuetifyjs.com/images/john.jpg'
+              "
               alt="John"
               width="100%"
             />
           </v-avatar>
         </v-row>
         <v-row>
-          <Card style="padding-top: 8em; margin-top: -5em">
+          <Card v-if="user" style="padding-top: 8em; margin-top: -5em">
             <v-row class="ml-12">
               <p>Name :</p>
               <v-spacer />
-              <p class="mr-16">{{ user.name ? user.name : '-' }}</p>
+              <p v-if="user.name" class="mr-16">{{ user.name }}</p>
+              <p v-if="!user.name" class="mr-16">-</p>
             </v-row>
             <v-row class="ml-12">
               <p>Gender :</p>
               <v-spacer />
-              <p class="mr-16">{{ user.gender ? user.gender : '-' }}</p>
+              <p v-if="user.gender" class="mr-16">{{ user.gender }}</p>
+              <p v-if="!user.gender" class="mr-16">-</p>
             </v-row>
             <v-row class="ml-12">
               <p>Birthday :</p>
               <v-spacer />
-              <p class="mr-16">{{ user.birthday ? user.birthday : '-' }}</p>
+              <p v-if="user.birthday" class="mr-16">{{ user.birthday }}</p>
+              <p v-if="!user.birthday" class="mr-16">-</p>
             </v-row>
             <v-row class="ml-12">
               <p>Hometown :</p>
               <v-spacer />
-              <p class="mr-16">{{ user.hometown ? user.hometown : '-' }}</p>
+              <p v-if="!user.hometown" class="mr-16">{{ user.hometown }}</p>
+              <p v-if="!user.hometown" class="mr-16">-</p>
             </v-row>
             <v-row class="ml-12">
               <p>Bio :</p>
               <v-spacer />
-              <p class="mr-16">{{ user.bio ? user.bio : '-' }}</p>
+              <p v-if="!user.bio" class="mr-16">{{ user.bio }}</p>
+              <p v-if="!user.bio" class="mr-16">-</p>
             </v-row>
             <v-btn
               block
@@ -63,28 +71,31 @@
         </v-row>
       </v-col>
       <v-col>
-        <Card class="pt-4">
+        <Card v-if="career" class="pt-4">
           <h1 class="mb-8">Career</h1>
           <v-row class="ml-12">
             <p>Company :</p>
             <v-spacer />
-            <p class="mr-16">
-              {{ user.career.company_name ? user.career.company_name : '-' }}
+            <p v-if="!career.company_name" class="mr-16">
+              {{ career.company_name }}
             </p>
+            <p v-if="!user.company_name" class="mr-16">-</p>
           </v-row>
           <v-row class="ml-12">
             <p>Starting From :</p>
             <v-spacer />
-            <p class="mr-16">
-              {{ user.career.starting_from ? user.career.starting_from : '-' }}
+            <p v-if="!career.starting_from" class="mr-16">
+              {{ career.starting_from }}
             </p>
+            <p v-if="!user.starting_from" class="mr-16">-</p>
           </v-row>
           <v-row class="ml-12">
             <p>Ending In :</p>
             <v-spacer />
-            <p class="mr-16">
-              {{ user.career.ending_in ? user.career.ending_in : '-' }}
+            <p v-if="!career.ending_in" class="mr-16">
+              {{ career.ending_in }}
             </p>
+            <p v-if="!user.ending_in" class="mr-16">-</p>
           </v-row>
           <v-btn
             block
@@ -96,27 +107,23 @@
         </Card>
       </v-col>
       <v-col class="mr-n3">
-        <Card class="pt-4">
+        <Card v-if="education" class="pt-4">
           <h1 class="mb-8">Education</h1>
-          <v-row class="ml-12">
+          <v-row class="ml-10">
             <p>School Name :</p>
             <v-spacer />
-            <p class="mr-16">
-              {{
-                user.education.school_name ? user.education.school_name : '-'
-              }}
+            <p v-if="!education.school_name" class="mr-16">
+              {{ education.school_name }}
             </p>
+            <p v-if="!education.school_name" class="mr-16">-</p>
           </v-row>
-          <v-row class="ml-12">
+          <v-row class="ml-10">
             <p>Graduation Time :</p>
             <v-spacer />
-            <p class="mr-16">
-              {{
-                user.education.graduation_time
-                  ? user.education.graduation_time
-                  : '-'
-              }}
+            <p v-if="!education.graduation_time" class="mr-16">
+              {{ education.graduation_time }}
             </p>
+            <p v-if="!education.graduation_time" class="mr-16">-</p>
           </v-row>
           <v-btn
             block
@@ -147,7 +154,18 @@ export default {
     user() {
       return this.$store.get('profile/user')
     },
+    career() {
+      return this.$store.get('profile/user').career
+    },
+    education() {
+      return this.$store.get('profile/user').education
+    },
   },
+  // watch:{
+  //   user(){
+
+  //   }
+  // },
   mounted() {
     this.load()
   },
