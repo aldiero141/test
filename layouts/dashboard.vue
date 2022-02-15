e39bd4cbaae33cbc3fbf1739e88390827d05c677
aldiero141 <rivelinoaldo@gmail.com>
2022-02-15 14:15:34 +0700

    feat: remove cookies on logout

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -3,8 +3,8 @@
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      
-      <v-btn to="/" color="red">Logout</v-btn>
+
+      <v-btn color="red" @click.stop="handlerLogOut">Logout</v-btn>
     </v-app-bar>
 
     <v-main>
@@ -12,7 +12,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-   
+
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -32,6 +32,12 @@ export default {
       rightDrawer: false,
       title: 'Nuxt Test',
     }
+  },
+  methods: {
+    handlerLogOut() {
+      this.$cookies.remove('access_token')
+      this.$router.push('/')
+    },
   },
 }
 </script>
